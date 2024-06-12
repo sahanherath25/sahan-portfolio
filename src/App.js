0769ce806656fcc27871d769f7a24ce3bb561a0a
Sahan Udara <sahanherath444@gmail.com>
2024-06-12 12:01:43 +0530

    Routes added with pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./pages/Contact";
+import Courses from "./pages/Courses";
+import Projects from "./pages/Projects";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>My Portfolio</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} exact element={<Home/>}/>
+                <Route path={"/projects"} exact element={<Projects/>}/>
+                <Route path={"/courses"} exact element={<Courses/>}/>
+                <Route path={"/contact"} exact element={<Contact/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
